36ea2fd273bcf95965f2b3d1912a5921162b7759
Charlestown <cdcunha@gmail.com>
2025-06-11 23:35:29 -0700

    Fixing errors during deploy on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Enable the webpack configuration to handle SVG files
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-  // Configure images for static export
-  images: {
-    unoptimized: true,
-  },
-  // Enable CSS modules
-  experimental: {
-    appDir: true,
-  },
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      appDir: true,
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        use: ['@svgr/webpack'],
+      });
+      return config;
+    },
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
+  
